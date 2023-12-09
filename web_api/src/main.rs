7ffe5c5c503fb54195e505e4e74f598104ac9fb0
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-09 12:27:09 +0000

    web api for login and create account routes

--- web_api/src/main.rs
+++ web_api/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new()
+        .route("/login", get(login))
+        .route("/new", get(create_account));
+
+    let listener = TcpListener::bind("localhost:3000")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, router)
+        .await
+        .unwrap();
+}
+
+async fn login() -> &'static str {
+    "Log In Route"
+}
+
+async fn create_account() -> &'static str {
+    "New Account Route"
 }
